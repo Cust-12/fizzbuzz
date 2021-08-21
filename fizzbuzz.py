0afead4759be8a1afe06d37433ba384a7c8d02ee
Cust-12 <84593579+Cust-12@users.noreply.github.com>
2021-08-21 15:26:58 +0800

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 21 15:12:36 2021
+
+@author: roryc
+"""
+
+for i in range(1,101) :             #prints numbers from 1 to 100
+    if i % 3 == 0 and i % 5 == 0:   #if the number is divisible by 3 and 5
+        print('fizzbuzz')           #print out fizzbuzz
+    elif i % 3 == 0:                #if the number is divisible by 3
+        print('fizz')               #print out fizz
+    elif i % 5 == 0:                #if the number is divisible by 5
+        print('buzz')               #print out buzz
+    else:                           #else just print out the number
+        print(i)
